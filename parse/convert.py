83604c499bb69378d6f7bbcbcac857fb79b1b2fa
Zaharia Klimkovich <xanax01dd@gmail.com>
2024-07-13 19:47:00 +0300

    upd: started work with xlsx

--- parse/convert.py
+++ parse/convert.py
@@ -2,7 +2,7 @@ from downloadTables import get_links,downloadFiles
 from pdftoxlsx import parsePDFandSave
 """TODO:Write to UMO (УМО//Учебно-методический отдел) and find out from whom i can get schedules in xlsx format
         Remake it to xlsx
-        Make this shit work
+        Make this shit work (it's work, but i'm lazy...)
 """
 print('Stage 1: Downloading PDF files')
 downloadedFilesList = downloadFiles(get_links())
