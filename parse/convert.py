4ae7b52d8e690cb2d30cf6cf5f3b7b5c7db1e1d0
Zaharia Klimkovich <xanax01dd@gmail.com>
2024-07-13 17:39:56 +0300

    upd: added todo for me (because i can forgot about it)

--- parse/convert.py
+++ parse/convert.py
@@ -1,6 +1,9 @@
 from downloadTables import get_links,downloadFiles
 from pdftoxlsx import parsePDFandSave
-
+"""TODO:Write to UMO (УМО//Учебно-методический отдел) and find out from whom i can get schedules in xlsx format
+        Remake it to xlsx
+        Make this shit work
+"""
 print('Stage 1: Downloading PDF files')
 downloadedFilesList = downloadFiles(get_links())
 print('Stage 2: Convering PDF files to XLSX')
