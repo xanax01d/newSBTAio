c05551af7304814a0915dc2a6ab06f6fa147ea26
Zaharia Klimkovich <xanax01dd@gmail.com>
2024-07-31 19:05:04 +0300

    formated file

--- utils/callback.py
+++ utils/callback.py
@@ -1,12 +1,13 @@
 from aiogram.filters.callback_data import CallbackData
 
+
 class UserInfo(CallbackData, prefix='UserInfo', sep='|'):
-    '''
+    """
     Class for collecting information about user selection in bot
-    '''
-    stage: str | None = None #Stage that user selected
-    course: str | None = None #Course that user selected
-    group: str | None = None #Group that user selected
-    day: int | None = None #Day that user selected
-    level: int | None = 0 #Level of menu (really)
-    captcha:str | None = None #for captcha
+    """
+    stage: str | None = None  #Stage that user selected
+    course: str | None = None  #Course that user selected
+    group: str | None = None  #Group that user selected
+    day: int | None = None  #Day that user selected
+    level: int | None = 0  #Level of menu (really)
+    captcha: str | None = None  #for captcha
